a6070e83c141086ae35252793b672fdeebe500c0
Dmitriy Kamay <kamay.dmitriy@gmail.com>
2025-02-04 02:12:10 +0300

    Lesson 22 Add Messages scroll

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import s from './Dialogs.module.css';
+import { NavLink } from "react-router-dom";
 
 const Dialogs = (props) => {
     return (
@@ -7,6 +8,7 @@ const Dialogs = (props) => {
         <div className={s.dialogsItems}>
           <h2 className={s.title}>Dialogs</h2>
           <div className={s.dialog}>
+          <NavLink to="/dialogs/1" className={s.dialogLink} activeClassName={s.active}></NavLink>
             <img className={s.dialogAva} src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" />
             <div className={s.dialogBody}>
               <div className={s.dialogName}>Сергей Петушенко</div>
@@ -14,6 +16,7 @@ const Dialogs = (props) => {
             </div>
           </div>
           <div className={s.dialog}>
+            <NavLink to="/dialogs/2" className={s.dialogLink} activeClassName={s.active}></NavLink>
             <img className={s.dialogAva} src="https://s1.bloknot-novorossiysk.ru/thumb/850x0xcut/upload/iblock/e84/omyb57mq3bau1kef8z8r9cjxv89x032m/WhatsApp-Image-2023_08_08-at-12.39.20.jpeg" alt="" />
             <div className={s.dialogBody}>
               <div className={s.dialogName}>Лёша Вонючка</div>
@@ -28,6 +31,90 @@ const Dialogs = (props) => {
               <img src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" className={s.messageAva} />
               <div className={s.messageText}>
                 Едем на дачу?
+              </div>
+            </div>
+            <div className={s.message}>
+              <img src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" className={s.messageAva} />
+              <div className={s.messageText}>
+                Едем на дачу?
+              </div>
+            </div>
+            <div className={s.message}>
+              <img src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" className={s.messageAva} />
+              <div className={s.messageText}>
+                Едем на дачу?
+              </div>
+            </div>
+            <div className={s.message}>
+              <img src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" className={s.messageAva} />
+              <div className={s.messageText}>
+                Едем на дачу?
+              </div>
+            </div>
+            <div className={s.message}>
+              <img src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" className={s.messageAva} />
+              <div className={s.messageText}>
+                Едем на дачу?
+              </div>
+            </div>
+            <div className={s.message}>
+              <img src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" className={s.messageAva} />
+              <div className={s.messageText}>
+                Едем на дачу?
+              </div>
+            </div>
+            <div className={s.message}>
+              <img src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" className={s.messageAva} />
+              <div className={s.messageText}>
+                Едем на дачу?
+              </div>
+            </div>
+            <div className={s.message}>
+              <img src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" className={s.messageAva} />
+              <div className={s.messageText}>
+                Едем на дачу?
+              </div>
+            </div>
+            <div className={s.message}>
+              <img src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" className={s.messageAva} />
+              <div className={s.messageText}>
+                Едем на дачу?
+              </div>
+            </div>
+            <div className={s.message}>
+              <img src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" className={s.messageAva} />
+              <div className={s.messageText}>
+                Едем на дачу?
+              </div>
+            </div>
+            <div className={s.message}>
+              <img src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" className={s.messageAva} />
+              <div className={s.messageText}>
+                Едем на дачу?
+              </div>
+            </div>
+            <div className={s.message}>
+              <img src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" className={s.messageAva} />
+              <div className={s.messageText}>
+                Едем на дачу?
+              </div>
+            </div>
+            <div className={s.message}>
+              <img src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" className={s.messageAva} />
+              <div className={s.messageText}>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam reprehenderit numquam ipsam voluptatem amet. Tempore iste voluptates harum qui sequi in vel, voluptas numquam pariatur expedita dolorum cupiditate, laborum similique.lore Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse laudantium est voluptatibus eum, rem culpa. Accusantium odit fuga illo! Quae nostrum quasi culpa consequuntur molestiae odio dolore placeat veritatis fugit?
+              </div>
+            </div>
+            <div className={s.message}>
+              <img src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" className={s.messageAva} />
+              <div className={s.messageText}>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam reprehenderit numquam ipsam voluptatem amet. Tempore iste voluptates harum qui sequi in vel, voluptas numquam pariatur expedita dolorum cupiditate, laborum similique.lore Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse laudantium est voluptatibus eum, rem culpa. Accusantium odit fuga illo! Quae nostrum quasi culpa consequuntur molestiae odio dolore placeat veritatis fugit?
+              </div>
+            </div>
+            <div className={s.message}>
+              <img src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" className={s.messageAva} />
+              <div className={s.messageText}>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam reprehenderit numquam ipsam voluptatem amet. Tempore iste voluptates harum qui sequi in vel, voluptas numquam pariatur expedita dolorum cupiditate, laborum similique.lore Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse laudantium est voluptatibus eum, rem culpa. Accusantium odit fuga illo! Quae nostrum quasi culpa consequuntur molestiae odio dolore placeat veritatis fugit?
               </div>
             </div>
           </div>
