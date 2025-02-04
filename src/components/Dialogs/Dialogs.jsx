f145933c9492b5b088edd00e02e5c431931b5055
Dmitriy Kamay <kamay.dmitriy@gmail.com>
2025-02-04 16:52:15 +0300

    Lesson 23 Props for Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,129 +2,54 @@ import React from "react";
 import s from './Dialogs.module.css';
 import { NavLink } from "react-router-dom";
 
+
+const DialogItem = (props) => {
+  let path = "/dialogs/" + props.id;
+
+  return (
+    <div className={s.dialog}>
+      <NavLink to={path} className={s.dialogLink} activeClassName={s.active}></NavLink>
+      <img className={s.dialogAva} src={props.imgSrc} alt="" />
+      <div className={s.dialogBody}>
+        <div className={s.dialogName}>{props.name}</div>
+        <div className={s.msgPreview}>{props.lastMsg}</div>
+      </div>
+    </div>
+  )
+}
+
+const MessageItem = (props) => {
+  return (
+    <div className={s.message}>
+      <img src={props.imgSrc} alt="" className={s.messageAva} />
+      <div className={s.messageText}>
+        {props.message}
+      </div>
+    </div>
+  )
+}
+
 const Dialogs = (props) => {
-    return (
-      <main className={s.dialogs}>
-        <div className={s.dialogsItems}>
-          <h2 className={s.title}>Dialogs</h2>
-          <div className={s.dialog}>
-          <NavLink to="/dialogs/1" className={s.dialogLink} activeClassName={s.active}></NavLink>
-            <img className={s.dialogAva} src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" />
-            <div className={s.dialogBody}>
-              <div className={s.dialogName}>Сергей Петушенко</div>
-              <div className={s.msgPreview}>Поехали на дачу охотиться за мышами, пить молоко, сосать сиску у коровок, гонять собак, лизать свою шерсть и делать котят.</div>
-            </div>
-          </div>
-          <div className={s.dialog}>
-            <NavLink to="/dialogs/2" className={s.dialogLink} activeClassName={s.active}></NavLink>
-            <img className={s.dialogAva} src="https://s1.bloknot-novorossiysk.ru/thumb/850x0xcut/upload/iblock/e84/omyb57mq3bau1kef8z8r9cjxv89x032m/WhatsApp-Image-2023_08_08-at-12.39.20.jpeg" alt="" />
-            <div className={s.dialogBody}>
-              <div className={s.dialogName}>Лёша Вонючка</div>
-              <div className={s.msgPreview}>ПОШЛИ В ДОТУ БЫСТРЕЕ</div>
-            </div>
-          </div>
+  return (
+    <main className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <h2 className={s.title}>Dialogs</h2>
+        <DialogItem id='1' name="Сергей Петушенко" lastMsg="Поехали на дачу охотиться за мышами, пить молоко, сосать сиску у коровок, гонять собак, лизать свою шерсть и делать котят" imgSrc="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" />
+        <DialogItem id='2' name="Лёша Вонючка" lastMsg="ПОГНАЛИ В ДОТУ" imgSrc="https://s1.bloknot-novorossiysk.ru/thumb/850x0xcut/upload/iblock/e84/omyb57mq3bau1kef8z8r9cjxv89x032m/WhatsApp-Image-2023_08_08-at-12.39.20.jpeg" />
+        <DialogItem id='3' name="Даша Рублёва" lastMsg="Приходи сегодня за гаражи" imgSrc="https://i.ytimg.com/vi/Deazgd2DRlI/maxresdefault.jpg" />
+      </div>
+      <div className={s.messages}>
+        <h2 className={s.title}>Messages</h2>
+        <div className={s.messagesArea}>
+          <MessageItem message="Поехали на дачу охотиться за мышами, пить молоко, сосать сиску у коровок, гонять собак, лизать свою шерсть и делать котят" imgSrc="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" />
         </div>
-        <div className={s.messages}>
-          <h2 className={s.title}>Messages</h2>
-          <div className={s.messagesArea}>
-            <div className={s.message}>
-              <img src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" className={s.messageAva} />
-              <div className={s.messageText}>
-                Едем на дачу?
-              </div>
-            </div>
-            <div className={s.message}>
-              <img src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" className={s.messageAva} />
-              <div className={s.messageText}>
-                Едем на дачу?
-              </div>
-            </div>
-            <div className={s.message}>
-              <img src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" className={s.messageAva} />
-              <div className={s.messageText}>
-                Едем на дачу?
-              </div>
-            </div>
-            <div className={s.message}>
-              <img src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" className={s.messageAva} />
-              <div className={s.messageText}>
-                Едем на дачу?
-              </div>
-            </div>
-            <div className={s.message}>
-              <img src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" className={s.messageAva} />
-              <div className={s.messageText}>
-                Едем на дачу?
-              </div>
-            </div>
-            <div className={s.message}>
-              <img src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" className={s.messageAva} />
-              <div className={s.messageText}>
-                Едем на дачу?
-              </div>
-            </div>
-            <div className={s.message}>
-              <img src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" className={s.messageAva} />
-              <div className={s.messageText}>
-                Едем на дачу?
-              </div>
-            </div>
-            <div className={s.message}>
-              <img src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" className={s.messageAva} />
-              <div className={s.messageText}>
-                Едем на дачу?
-              </div>
-            </div>
-            <div className={s.message}>
-              <img src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" className={s.messageAva} />
-              <div className={s.messageText}>
-                Едем на дачу?
-              </div>
-            </div>
-            <div className={s.message}>
-              <img src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" className={s.messageAva} />
-              <div className={s.messageText}>
-                Едем на дачу?
-              </div>
-            </div>
-            <div className={s.message}>
-              <img src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" className={s.messageAva} />
-              <div className={s.messageText}>
-                Едем на дачу?
-              </div>
-            </div>
-            <div className={s.message}>
-              <img src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" className={s.messageAva} />
-              <div className={s.messageText}>
-                Едем на дачу?
-              </div>
-            </div>
-            <div className={s.message}>
-              <img src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" className={s.messageAva} />
-              <div className={s.messageText}>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam reprehenderit numquam ipsam voluptatem amet. Tempore iste voluptates harum qui sequi in vel, voluptas numquam pariatur expedita dolorum cupiditate, laborum similique.lore Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse laudantium est voluptatibus eum, rem culpa. Accusantium odit fuga illo! Quae nostrum quasi culpa consequuntur molestiae odio dolore placeat veritatis fugit?
-              </div>
-            </div>
-            <div className={s.message}>
-              <img src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" className={s.messageAva} />
-              <div className={s.messageText}>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam reprehenderit numquam ipsam voluptatem amet. Tempore iste voluptates harum qui sequi in vel, voluptas numquam pariatur expedita dolorum cupiditate, laborum similique.lore Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse laudantium est voluptatibus eum, rem culpa. Accusantium odit fuga illo! Quae nostrum quasi culpa consequuntur molestiae odio dolore placeat veritatis fugit?
-              </div>
-            </div>
-            <div className={s.message}>
-              <img src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" alt="" className={s.messageAva} />
-              <div className={s.messageText}>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam reprehenderit numquam ipsam voluptatem amet. Tempore iste voluptates harum qui sequi in vel, voluptas numquam pariatur expedita dolorum cupiditate, laborum similique.lore Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse laudantium est voluptatibus eum, rem culpa. Accusantium odit fuga illo! Quae nostrum quasi culpa consequuntur molestiae odio dolore placeat veritatis fugit?
-              </div>
-            </div>
-          </div>
-          <form action="" className={s.newMessage}>
-            <textarea className={s.newMessageArea} placeholder="Write message" name="newMessageArea" id="newMessageArea" />
-            <button className={s.sendMessage}>Send message</button>
-          </form>
-        </div>
-      </main>
-    )
+        <form action="" className={s.newMessage}>
+          <textarea className={s.newMessageArea} placeholder="Write message" name="newMessageArea" id="newMessageArea" />
+          <button className={s.sendMessage}>Send message</button>
+        </form>
+      </div>
+    </main>
+  )
 }
 
 export default Dialogs;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,32 +1,15 @@
 import React from "react";
 import s from './Profile.module.css';
 import MyPosts from "./MyPosts/MyPosts";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
-const Profile = () => {
-    return (
-      <main className={s.profile}>
-      <div className={s.wallpaperWrapper}>
-        <img src="https://niti-d.by/wp-content/uploads/2020/10/15.1.jpg" alt="" className={s.wallpaper} />
-      </div>
-      <div className={s.wrapper}>
-        <div className={s.mainBlock}>
-        <img src="https://i.ibb.co/CDYzqYW/2025-01-29-222000471.png" alt="" className={s.ava} />
-        <div className={s.info}>
-          <div className={s.name}>
-            Валерий Лохматый
-          </div>
-          <div className={s.desc}>
-            <div className={s.descItem}>Date of Birth: 6 may 2020</div>
-            <div className={s.descItem}>City: Minsk</div>
-            <div className={s.descItem}>Education: BSPU'18</div>
-            <a href="#" className={s.descItem}>https://doggyaquatrip.ru</a>
-          </div>
-        </div>
-        </div>
-        <MyPosts />
-      </div>
+const Profile = (props) => {
+  return (
+    <main className={s.profile}>
+      <ProfileInfo />
+      <MyPosts />
     </main>
-    )
+  )
 }
 
 export default Profile;
