8dbc773ada7a7ab0d57cfef1507526c2ce536ba0
Dmitriy Kamay <kamay.dmitriy@gmail.com>
2025-02-11 02:56:27 +0300

    Lesson 51 Add axios and GET request for users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import s from './Users.module.css';
 import UserItem from "./UserElement/UserItem";
+import * as axios from 'axios';
+
+
+
 
 const Users = (props) => {
+  if (props.state.users.length === 0) {
+    axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+      console.log(response.data.items);
+      props.setUsers(response.data.items);
+    });
+  }
   const search = React.createRef();
   const onUpdateSearch = () => {
     console.log(search.current.value)
     props.updateSearch(search.current.value);
   }
-  let userElements = props.state.findedUsers.map(user => <UserItem toggleFriend={props.toggleFriend} id={user.user_id} name={user.name} key={user.user_id} imgSrc={user.avatarSrc} isFriend={user.isFriend} city={user.city} />)
+  let userElements = props.state.findedUsers.map(user => <UserItem toggleFriend={props.toggleFriend} id={user.id} name={user.name} key={user.user_id} imgSrc={user.avatarSrc} isFriend={user.followed} city={user.city} />)
   return (
     <search className={s.findUsers}>
       <h1 className={s.title}>Users</h1>
