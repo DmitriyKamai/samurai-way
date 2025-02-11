255bbdbf90ff9c0cca2e905c2bc3b6abadfab747
Dmitriy Kamay <kamay.dmitriy@gmail.com>
2025-02-11 17:13:48 +0300

    Lesson 53 Users refactored to classes component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,31 +3,31 @@ import s from './Users.module.css';
 import UserItem from "./UserElement/UserItem";
 import * as axios from 'axios';
 
-
-
-
-const Users = (props) => {
-  if (props.state.users.length === 0) {
-    axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-      console.log(response.data.items);
-      props.setUsers(response.data.items);
-    });
+class Users extends React.Component {
+  constructor(props) {
+    super(props)
+    this.search = React.createRef();
+    axios.get('https://social-network.samuraijs.com/api/1.0/users')
+        .then(response => {
+          this.props.setUsers(response.data.items);
+        });
   }
-  const search = React.createRef();
-  const onUpdateSearch = () => {
-    console.log(search.current.value)
-    props.updateSearch(search.current.value);
+
+  onUpdateSearch = () => {
+    this.props.updateSearch(this.search.current.value);
   }
-  let userElements = props.state.findedUsers.map(user => <UserItem toggleFriend={props.toggleFriend} id={user.id} name={user.name} key={user.user_id} imgSrc={user.avatarSrc} isFriend={user.followed} city={user.city} />)
-  return (
-    <search className={s.findUsers}>
-      <h1 className={s.title}>Users</h1>
-      <input onChange={onUpdateSearch} type="text" ref={search} value={props.state.searchUserInfo} className={s.search} placeholder="Write name"></input>
-      <div className={s.userList}>
-        {userElements}
-      </div>
-    </search>
-  )
+
+  render() {
+    return (
+      <search className={s.findUsers}>
+        <h1 className={s.title}>Users</h1>
+        <input onChange={this.onUpdateSearch} type="text" ref={this.search} value={this.props.state.searchUserInfo} className={s.search} placeholder="Write name"></input>
+        <div className={s.userList}>
+          {this.props.state.findedUsers.map(user => <UserItem toggleFriend={this.props.toggleFriend} id={user.id} name={user.name} key={user.user_id} imgSrc={user.avatarSrc} isFriend={user.followed} city={user.city} />)}
+        </div>
+      </search>
+    )
+  }
 }
 
 export default Users;
